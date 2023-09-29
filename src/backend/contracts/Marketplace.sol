be93be6cb29c4b4950975239dc7233cc90935c08
Deepan Kumar <deepanrvdvenu@gmail.com>
2023-09-29 09:55:35 +0530

    contract added

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -1,102 +1,128 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+contract NFTMarketplace is ERC721Enumerable, Ownable {
 
-import "hardhat/console.sol";
+    struct NFT {
+        string name;
+        string tokenURI;
+        address owner;
+        uint256 price;
+        uint256 tokenId;
+        bool isListed;
 
-contract Marketplace is ReentrancyGuard {
-
-    // Variables
-    address payable public immutable feeAccount; // the account that receives fees
-    uint public immutable feePercent; // the fee percentage on sales 
-    uint public itemCount; 
-
-    struct Item {
-        uint itemId;
-        IERC721 nft;
-        uint tokenId;
-        uint price;
-        address payable seller;
-        bool sold;
     }
 
-    // itemId -> Item
-    mapping(uint => Item) public items;
+    NFT[] public nfts;
 
-    event Offered(
-        uint itemId,
-        address indexed nft,
-        uint tokenId,
-        uint price,
-        address indexed seller
-    );
-    event Bought(
-        uint itemId,
-        address indexed nft,
-        uint tokenId,
-        uint price,
-        address indexed seller,
-        address indexed buyer
-    );
+    mapping(uint256 => uint256) public nftIndexToTokenId;
+    mapping(uint256 => address) public nftIndexToOwner;
+    mapping(string => bool) public usedTokenURIs;
 
-    constructor(uint _feePercent) {
-        feeAccount = payable(msg.sender);
-        feePercent = _feePercent;
+    constructor() ERC721("NFT Marketplace", "NFTM") {}
+
+
+    function mintNFT(string memory _name, string memory _tokenURI,uint price) external returns(uint) {
+        require(!usedTokenURIs[_tokenURI], "Token URI already used"); // Check if URI is unique
+
+        uint256 tokenId = nfts.length;
+        _mint(msg.sender, tokenId);
+        nfts.push(NFT(_name, _tokenURI, msg.sender, 0, tokenId,false));
+        nftIndexToTokenId[tokenId] = tokenId;
+        nftIndexToOwner[tokenId] = msg.sender;
+        usedTokenURIs[_tokenURI] = true; // Mark URI as used
+        listNFTForSale(tokenId,price);
+        return tokenId;
+
     }
 
-    // Make item to offer on the marketplace
-    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
-        require(_price > 0, "Price must be greater than zero");
-        // increment itemCount
-        itemCount ++;
-        // transfer nft
-        _nft.transferFrom(msg.sender, address(this), _tokenId);
-        // add new item to items mapping
-        items[itemCount] = Item (
-            itemCount,
-            _nft,
-            _tokenId,
-            _price,
-            payable(msg.sender),
-            false
-        );
-        // emit Offered event
-        emit Offered(
-            itemCount,
-            address(_nft),
-            _tokenId,
-            _price,
-            msg.sender
-        );
+    
+     function listNFTForSale(uint256 _tokenId, uint256 _price) public {
+        require(exists(_tokenId), "NFT does not exist");
+        require(ownerOf(_tokenId) == msg.sender, "Only the owner can list");
+        nfts[_tokenId].price = _price;
+        nfts[_tokenId].isListed = true;
     }
 
-    function purchaseItem(uint _itemId) external payable nonReentrant {
-        uint _totalPrice = getTotalPrice(_itemId);
-        Item storage item = items[_itemId];
-        require(_itemId > 0 && _itemId <= itemCount, "item doesn't exist");
-        require(msg.value >= _totalPrice, "not enough ether to cover item price and market fee");
-        require(!item.sold, "item already sold");
-        // pay seller and feeAccount
-        item.seller.transfer(item.price);
-        feeAccount.transfer(_totalPrice - item.price);
-        // update item to sold
-        item.sold = true;
-        // transfer nft to buyer
-        item.nft.transferFrom(address(this), msg.sender, item.tokenId);
-        // emit Bought event
-        emit Bought(
-            _itemId,
-            address(item.nft),
-            item.tokenId,
-            item.price,
-            item.seller,
-            msg.sender
-        );
+    function exists(uint256 tokenId) internal view returns (bool) {
+    return tokenId < nfts.length;
+}
+
+   
+
+    function purchaseNFT(uint256 _tokenId) external payable {
+        require(exists(_tokenId), "NFT does not exist");
+        require(nfts[_tokenId].isListed, "NFT is not listed for sale");
+        require(msg.value >= nfts[_tokenId].price, "Insufficient funds");
+
+        address seller = ownerOf(_tokenId);
+        nfts[_tokenId].isListed = false;
+        nfts[_tokenId].owner = msg.sender;
+
+        payable(seller).transfer(msg.value);
+
+        _transfer(seller, msg.sender, _tokenId);
     }
-    function getTotalPrice(uint _itemId) view public returns(uint){
-        return((items[_itemId].price*(100 + feePercent))/100);
+
+    function viewAllNFTsForSale() external view returns (NFT[] memory) {
+        uint256 totalListed = 0;
+        for (uint256 i = 0; i < nfts.length; i++) {
+            if (nfts[i].isListed) {
+                totalListed++;
+            }
+        }
+
+        NFT[] memory listedNFTs = new NFT[](totalListed);
+        uint256 index = 0;
+        for (uint256 i = 0; i < nfts.length; i++) {
+            if (nfts[i].isListed) {
+                listedNFTs[index] = nfts[i];
+                index++;
+            }
+        }
+
+        return listedNFTs;
+    }
+
+    function viewmylistednfts()public view returns(NFT[] memory){
+        NFT[] memory a = viewOwnedNFTs(msg.sender);
+        NFT[] memory b = new NFT[](a.length);
+        uint index=0;
+        for(uint i=0;i<a.length;i++){
+                if(nfts[i].isListed && nfts[i].owner == msg.sender){
+                    b[index] = a[i];
+                    index++;
+                }
+        }
+        return b;
+    }
+
+    function viewOwnedNFTs(address _owner) public view returns (NFT[] memory) {
+        uint256 totalOwned = balanceOf(_owner);
+        NFT[] memory ownedNFTs = new NFT[](totalOwned);
+        uint256 index = 0;
+        for (uint256 i = 0; i < nfts.length; i++) {
+            if (ownerOf(i) == _owner) {
+                ownedNFTs[index] = nfts[i];
+                index++;
+            }
+        }
+        return ownedNFTs;
+    }
+
+    function viewTokenUri(uint tokenid) public view returns(string memory){
+        require(exists(tokenid), "NFT does not exist");
+        return nfts[tokenid].tokenURI;
+    }
+
+     function getNFTCount() public view returns (uint) {
+        return nfts.length;
     }
 }
+
+
+//https://ipfs.io/ipfs/QmW6Wn2PPE3roNspUBMGh5pEEqfcBYh2h7siDd5YYK3psG/1.json"
+        
